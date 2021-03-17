d4298aac221616713ac63bd63451145f2c32e3af
Ketan Pandey <ketanpandey.77200@gmail.com>
2021-03-17 10:58:07 +0530

    Assignment 1

---
+++ Assignment 1/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello World in PHP</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <h2>Welcome to the world of web Development</h2>
+</body>
+</html>
